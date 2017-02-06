f02eca41f3d6d1da90e31c3d344cc9acf2f2a95b
Xiaojing Quan <quanxj17@gmail.com>
2017-02-06 14:58:42 -0500

    Test Code

---
+++ Ottawa.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 08:21:28 2017
+
+@author: xquan
+"""
+
+# DOWNLOAD AND MANIPULATE DATA FOR OTTAWA AREA
+
+import ERA_Interim
+
+#set parameters
+
