22754135b4e53c1de3866730ae8cd708f48b9983
Xiaojing Quan <quanxj17@gmail.com>
2017-03-09 14:14:37 -0500

    Codes Reorganized and Updated-Continued

---
+++ Ottawa_Plot.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+#
+# Copyright Xiaojing Quan & Stephan Gruber
+# =============================================================================    
+# REVISION HISTORY 
+# 20170309 -- Initial version created 
+#
+#==============================================================================
+# An example for processing and analysing ERA_Interim data:
+# --Air Temperature at pressure levels 
+# --Relative Humidity 
+# --Wind Speed/Direction 
+# --Air Temperature at 2 meter 
+# --Suface Solar Radiation Downwards 
+# --Suface Thermal radiation Downwards 
+# --Total Precipitation
+# --Others
+#
+# Files Format: NetCDF
+#==============================================================================
+#PURPOSE 
+#To demonstrate how to read and write data with NetCDF files using a NetCDF file 
+# from ERA_Interim(ECMWF)
+# Plotting using Matplotlib and Basemap
+#==============================================================================
+# REFERENCE
+#
+#==============================================================================
+
+
+# Reading in variables
+
+
+
+
+
+
+
+#Creat the basemap object
+
+
+
+
+
+
+        
+
+
+#Populate a gridded array for plotting 
+
+
+
+
+
+
