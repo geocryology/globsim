4e293e74872b66c7dde0b7887dc473c3fe62d568
Xiaojing Quan <quanxj17@gmail.com>
2018-03-05 15:06:29 -0500

    Added ERADataCheck

--- data_check.py
+++ data_check.py
@@ -133,16 +133,129 @@ class MERRA2DataCheck(object):
         #                
         self.DataReadin(path.join(self.dir_inp,'merra_sr_*.nc'))          
 
+
+class ERADataCheck(object):
+    """
+        To check  the continourity of the time coverage of from all the downloaded 
+        netCDF files with a wildward expression
+          
+        Args:
+        ncfile_in: Full path to an MERRA-2 derived netCDF file. This can
+        contain wildcards to point to multiple files if temporal
+      
+    """
+
+    def __init__(self, ifile):
+        #read parameter file
+        self.ifile = ifile
+        par = ParameterIO(self.ifile)
+        self.dir_inp = path.join(par.project_directory,'eraint') 
+        self.dir_out = path.join(par.project_directory,'station')
+        self.variables = par.variables
+        self.list_name = par.list_name
+        self.stations_csv = path.join(par.project_directory,
+                                      'par', par.station_list)
         
+        #read station points 
+        self.stations = StationListRead(self.stations_csv)  
+        
+        # time bounds
+        self.date  = {'beg' : par.beg,
+                      'end' : par.end}
+                      
+                      
+    def DataReadin(self, file_in):    
+        """
+        To read in all the downloaded netCDF files with a wildward expression
+          
+        Args:
+            ncfile_in: Full path to an MERRA-2 derived netCDF file. This can
+                        contain wildcards to point to multiple files if temporal
+                        chunking was used.
+              
+            ncfile_out: Full path to the output netCDF file to write.  
+          """
+	  
+	# open netcdf file handle, can be one file of several with wildcards
+	ncf = nc.MFDataset(file_in, 'r', aggdim ='time') 
+                  
+        #get variable 'time' 
+        nctime = ncf.variables['time'][:]
+        t_unit = ncf.variables['time'].units 
+        try :
+            t_cal = ncf.variables['time'].calendar
+        except AttributeError : # Attribute doesn't exist
+            t_cal = u"gregorian" # or standard
+ 
+        # check the continourity of the time coverage
+        if file_in == path.join(self.dir_inp,'era_sf_*.nc'):
+             missingIndex = list(set(xrange(nctime[0], nctime[-1] + 1, 3)) - set(nctime))       
+             if missingIndex == []:
+                print "NO MISSING TIME FOR", file_in
+             else:    
+                 missingIndex.sort()
+                 missingTime = nc.num2date(missingIndex, units = t_unit, calendar = t_cal)
+                 
+                 print 'FOR', file_in
+                 
+                 print "Number of Missing Indices of Time : ", len(missingIndex)
+                 
+                 print "Unit of Time: ", t_unit
+                 
+                 print 'Missing Time List:', list(missingTime)              
+        
+            
+        else:
+             missingIndex = list(set(xrange(nctime[0], nctime[-1] + 1, 6)) - set(nctime))      
+             if missingIndex == []:
+                print "NO MISSING TIME FOR", file_in
+             else:    
+                 missingIndex.sort()
+                 missingTime = nc.num2date(missingIndex, units = t_unit, calendar = t_cal)
+
+                 print 'FOR', file_in
+
+                 print "Number of Missing Indices of Time : ", len(missingIndex)
+                 
+                 print "Unit of Time: ", t_unit
+
+                 print 'Missing Time List:', list(missingTime)              
+
+    def process(self):
+        """
+        combine the specific type of single or mutiple netCDF files
+        """                       
+
+        # === Surface  Data ===    
+        #                
+        self.DataReadin(path.join(self.dir_inp,'era_sa_*.nc'))          
+
+        # === Pressure-level Data ===    
+        # 
+        #                
+#        self.DataReadin(path.join(self.dir_inp,'era_pl_*.nc'))          
+
+        # === radiation Data ===    
+        # 
+        #                
+#        self.DataReadin(path.join(self.dir_inp,'era_sf_*.nc'))          
+
+                
 # ===========For Run DataCheck ================================================ 
 # Args:
 # ifile : Full path to a Globsim Interpolate Paramter file  
   
 ifile = '/home/xquan/src/globsim/examples/par/examples.globsim_interpolate'
 
+# merra2 data check
 MERRA2check = MERRA2DataCheck(ifile)
 
 MERRA2check.process()
+
+# eraint data check
+ERAcheck = ERADataCheck(ifile)
+
+ERAcheck.process()
 
 
                   
