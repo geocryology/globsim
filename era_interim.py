a0da8191ca317b01b8aa9609e6e149edb5cbc012
stgruber <stephan.gruber@carleton.ca>
2018-05-19 14:32:37 -0400

    Minor testing

--- era_interim.py
+++ era_interim.py
@@ -862,10 +862,11 @@ class ERAinterpolate(object):
         for n in range(niter):
             # indices
             beg = n * self.cs
+            # restrict last chunk to lenght of tmask plus one (to get last time)
             end = min(n*self.cs + self.cs, len(time_in))
-            if invariant:
+            #if invariant:
                 # allow topography to work in same code
-                end = 0
+             #   end = 0
             
             # time to make tmask for chunk 
             beg_time = nc.num2date(nctime[beg], units=t_unit, calendar=t_cal)
