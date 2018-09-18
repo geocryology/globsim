aeb971a25501d69b8eaacd5bdc74371dc33e51a3
nicholas512 <nicholas512@gmail.com>
2018-09-18 13:20:46 -0400

    Revert "converted all tabs to 4x spaces"
    This reverts commit 2dd760e5ec8eee37cde52b30c49da046a8a7bc59.

--- merra_2.py
+++ merra_2.py
@@ -1885,7 +1885,7 @@ class MERRAinterpolate(object):
         # regrid operation, create destination field (variables, times, points)
         dfield = regrid2D(sfield, dfield)        
         sfield.destroy() #free memory                  
-                    
+		            
         return dfield, variables
 
     def MERRA2station(self, ncfile_in, ncfile_out, points,
@@ -1977,12 +1977,12 @@ class MERRAinterpolate(object):
                 end_time = nc.num2date(time_in[end], units=t_unit, calendar=t_cal)
             
             # !! CAN'T HAVE '<= end_time', would damage appeding 
-        tmask_chunk = (time < end_time) * (time >= beg_time)
-        if invariant:
+	    tmask_chunk = (time < end_time) * (time >= beg_time)
+	    if invariant:
                 # allow topography to work in same code
                 tmask_chunk = [True]
            
-        # get the interpolated variables
+	    # get the interpolated variables
             dfield, variables = self.MERRA2interp2D(ncfile_in, ncf_in, self.stations, tmask_chunk,
                                     variables=None, date=None) 
 
@@ -1998,10 +1998,10 @@ class MERRAinterpolate(object):
                 try:
                     lev = ncf_in.variables['level'][:]
                     # dimension: time, level, latitude, longitude
-                    ncf_out.variables[var][beg:end,:,:] = dfield.data[i,:,:,:]                
+                    ncf_out.variables[var][beg:end,:,:] = dfield.data[i,:,:,:]    		    
                 except:
                     # time, latitude, longitude
-                    ncf_out.variables[var][beg:end,:] = dfield.data[i,:,:]            
+                    ncf_out.variables[var][beg:end,:] = dfield.data[i,:,:]		    
                                      
         #close the file
         ncf_in.close()
@@ -2120,15 +2120,15 @@ class MERRAinterpolate(object):
                     # pressure [hPa] variable from levels, shape: (time, level)
                     data = np.repeat([ncf.variables['level'][:]],
                                       len(time),axis=0).ravel()
-            ipol = data[va]*wa + data[vb]*wb   # interpolated value
-            #---------------------------------------------------------
-            #if mask[pixel] == false, pass the maximum of pressure level to pixles
-            level_highest = ncf.variables['level'][:][-1]
-            level_lowest = ncf.variables['level'][:][0]
-            for j, value in enumerate(ipol):
-                if value == level_highest:
-                ipol[j] = level_lowest
-                #---------------------------------------------------------                                                     
+		    ipol = data[va]*wa + data[vb]*wb   # interpolated value
+		    #---------------------------------------------------------
+		    #if mask[pixel] == false, pass the maximum of pressure level to pixles
+		    level_highest = ncf.variables['level'][:][-1]
+		    level_lowest = ncf.variables['level'][:][0]
+		    for j, value in enumerate(ipol):
+		        if value == level_highest:
+			    ipol[j] = level_lowest
+	            #---------------------------------------------------------	    	    				                     
                 else:    
                     #read data from netCDF
                     data = ncf.variables[var][:,:,n].ravel()
