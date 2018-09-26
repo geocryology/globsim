bff20ee2695c2e702764b39936bce370733a8c4e
Xiaojing Quan <quanxj17@gmail.com>
2018-09-26 09:17:49 -0400

    Some Changes_LW_MERRA_Wm2_topo_TBC

--- merra_2.py
+++ merra_2.py
@@ -2521,24 +2521,20 @@ class MERRAscale(object):
         https://www.geosci-model-dev.net/7/387/2014/gmd-7-387-2014.pdf
         """             
         # get sky view, and interpolate in time
-        # N = np.zeros((self.nt, self.nstation), dtype=np.float32)
-        # time_in = self.nc_sa.variables['time'][:].astype(np.int64)
         N = np.asarray(list(self.stations['sky_view'][:]))
-        # values = np.repeat(n,len(time_in), axis=0)
-        # for n,s in enumerate(self.rg.variables['station'][:].tolist()):
-        #     N[:, n] = series_interpolate(self.times_out_nc, 
-        #                                     time_in*3600, values[:, n])          
-        # compute                              
-        LW = LW_downward(self.rg.variables['RH_MERRA2_per_sur'][:, :],
-             self.rg.variables['AIRT_MERRA2_C_sur'][:, :], N[:])
+
+        # compute
+	for i in range(0, len(self.rg.variables['RH_MERRA2_per_sur'][:])):
+	    for n,s in enumerate(self.rg.variables['station'][:].tolist()):
+                LW = LW_downward(self.rg.variables['RH_MERRA2_per_sur'][i, n],
+                     self.rg.variables['AIRT_MERRA2_C_sur'][i, n]+273.15, N[n])
 
         # add variable to ncdf file
         vn = 'LW_MERRA2_Wm2_topo' # variable name
         var           = self.rg.createVariable(vn,'f4',('time', 'station'))    
         var.long_name = 'Incoming long-wave radiation MERRA-2 surface only'
         var.units     = 'W/m2'.encode('UTF8')
-        self.rg.variables[vn][:, :] = LW 
-
+        self.rg.variables[vn][:, :] = LW
 
 
 #==============================================================================    
