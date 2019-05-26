4435e15db85d384bbf0978d5fcf10b45eef90fd9
BC <caobin198912@outlook.com>
2019-05-26 14:44:05 +0800

    Update JRA.py

--- JRA.py
+++ JRA.py
@@ -1201,7 +1201,7 @@ class JRAinterpolate(object):
             # indices
             beg = n * self.cs
             # restrict last chunk to lenght of tmask plus one (to get last time)
-            end = min(n*self.cs + self.cs - 1, len(time_in)-1)
+            end = min(n*self.cs + self.cs, len(time_in)) -1
             
             # time to make tmask for chunk 
             beg_time = nc.num2date(time_in[beg], units=t_unit, calendar=t_cal)
