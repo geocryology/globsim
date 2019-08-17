fixed relateive humidity scaling
relateive humidity is scaled to [0-1]

JRA.py
@@ -1641,7 +1641,7 @@ class JRAscale(object):
         
         # interpolate station by station
         time_in = self.nc_sa.variables['time'][:]
-        values  = self.nc_sa.variables['Relative humidity'][:]                   
+        values  = self.nc_sa.variables['Relative humidity'][:]/100 #[1]                 
         for n, s in enumerate(self.rg.variables['station'][:].tolist()):  
             self.rg.variables[vn][:, n] = np.interp(self.times_out_nc, 
                                                     time_in, values[:, n])  

era_interim.py
@@ -1496,7 +1496,7 @@ class ERAIscale(object):
     def RH_per_sur(self):
         """
         Relative humdity derived from surface data, exclusively. Clipped to
-        range [0.1,99.9]. Kernel AIRT_ERAI_C_sur must be run before.
+        range [0.001, 0.999]. Kernel AIRT_ERAI_C_sur must be run before.
         """         
         # temporary variable,  interpolate station by station
         dewp = np.zeros((self.nt, self.nstation), dtype=np.float32)
@@ -1515,7 +1515,7 @@ class ERAIscale(object):
         
         # simple: https://doi.org/10.1175/BAMS-86-2-225
         RH = 100 - 5 * (self.rg.variables['AIRT_ERAI_C_sur'][:, :]-dewp[:, :])
-        self.rg.variables[vn][:, :] = RH.clip(min=0.1, max=99.9)    
+        self.rg.variables[vn][:, :] = RH.clip(min=0.1, max=99.9)/100
         
         
     def WIND_sur(self):