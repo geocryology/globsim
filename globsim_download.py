a0d7cfe6eb277d953feb6afd126e65315d051b79
hgmacc <59845773+hgmacc@users.noreply.github.com>
2020-08-18 17:03:37 -0400

    Updated changes from working

--- globsim_download.py
+++ globsim_download.py
@@ -33,11 +33,10 @@ if __name__ == "__main__":
     parser.add_argument('-f',             default=None, type=str, help="file path to download parameter file")
     parser.add_argument('-d',             default=None, type=str, nargs="*", help="What data sources should run? ERAI, ERA5, MERRA, JRA")
     parser.add_argument('-r', '--retry',  default=1,    type=int, help="Number of times to re-launch download if it crashes.")
-    parser.add_argument('-s', '--serial', action='store_true',    help="All data sources run on one process")
+    parser.add_argument('-m', '--multi', action='store_true',    help="Download all data sources simultaneously")
     
     args = parser.parse_args()
     
-    multi = False if args.serial else True
     pfile = args.f
     
     ERAI =  True if args.d is None or "ERAI"  in args.d else False
@@ -49,11 +48,11 @@ if __name__ == "__main__":
     i = 0
     
     if r_max <= 1:
-        GlobsimDownload(pfile, ERAI=ERAI, ERA5=ERA5, JRA=JRA, MERRA=MERRA, multithread=multi)
+        GlobsimDownload(pfile, ERAI=ERAI, ERA5=ERA5, JRA=JRA, MERRA=MERRA, multithread=args.multi)
     else:
         while i < r_max:
             try:
-                GlobsimDownload(pfile, ERAI=ERAI, ERA5=ERA5, JRA=JRA, MERRA=MERRA, multithread=multi)
+                GlobsimDownload(pfile, ERAI=ERAI, ERA5=ERA5, JRA=JRA, MERRA=MERRA, multithread=args.multi)
             except Exception as e:
                 print(e)
             time.sleep(360)
