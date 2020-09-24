be8b25e39b68576b0ea08b71f706fc0dda61577d
nicholas512 <15223327+nicholas512@users.noreply.github.com>
2020-09-24 10:49:12 -0400

    era-i

--- globsim/era_interim.py
+++ globsim/era_interim.py
@@ -853,6 +853,12 @@ class ERAIinterpolate(GenericInterpolate):
         rootgrp = netcdf_base(ncfile_out, len(height), nt, 'hours since 1900-01-01 00:00:0.0')
         rootgrp.source = 'ERA-Interim, interpolated (bi)linearly to stations'
        
+        time = rootgrp['time']
+        station = rootgrp['station']
+        latitude = rootgrp['latitude']
+        longitude = rootgrp['longitude']
+        height = rootgrp['height']
+
         # assign base variables
         time[:]      = ncf.variables['time'][:]
         station[:]   = ncf.variables['station'][:]
