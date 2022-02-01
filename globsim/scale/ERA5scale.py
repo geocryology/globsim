72 - Fix slow scaling (#92)
* add methods for building scaled time array

* JRA: scaled timesteps from new method in parent class

* refactor as more independent functions

* merra: scale timesteps with method in parent class

* set input time step in set_time_scale

* increment version

* use new methods in ERA5

* use new methods for ERAI

* linting

* bugfix

globsim/scale/ERA5scale.py
@@ -28,10 +28,9 @@ import numpy as np
 import urllib3
 import logging
 
-from datetime import timedelta
-from math import floor, atan2, pi
-from os import path
+from math import atan2, pi
 from scipy.interpolate import interp1d
+from pathlib import Path
 
 from globsim.common_utils import series_interpolate
 from globsim.meteorology import spec_hum_kgkg
@@ -40,6 +39,7 @@ from globsim.scale.GenericScale import GenericScale
 
 urllib3.disable_warnings()
 logger = logging.getLogger('globsim.scale')
+
 
 class ERA5scale(GenericScale):
     """
@@ -68,48 +68,26 @@ class ERA5scale(GenericScale):
             self.src = 'era5_ens'
 
         # input file handles
-        self.nc_pl = nc.Dataset(path.join(self.intpdir,
-                                          '{}_pl_'.format(self.src) +
-                                          self.list_name + '_surface.nc'), 'r')
-        self.nc_sa = nc.Dataset(path.join(self.intpdir,
-                                          '{}_sa_'.format(self.src) +
-                                self.list_name + '.nc'), 'r')
-        self.nc_sf = nc.Dataset(path.join(self.intpdir,
-                                          '{}_sf_'.format(self.src) +
-                                self.list_name + '.nc'), 'r')
-        self.nc_to = nc.Dataset(path.join(self.intpdir,
-                                          '{}_to_'.format(self.src) +
-                                self.list_name + '.nc'), 'r')
+        self.nc_pl = nc.Dataset(Path(self.intpdir, f"{self.src}_pl_{self.list_name}_surface.nc"),
+                                'r')
+        self.nc_sa = nc.Dataset(Path(self.intpdir, f'{self.src}_sa_{self.list_name}.nc'),
+                                'r')
+        self.nc_sf = nc.Dataset(Path(self.intpdir, f'{self.src}_sf_{self.list_name}.nc'),
+                                'r')
+        self.nc_to = nc.Dataset(Path(self.intpdir, f'{self.src}_to_{self.list_name}.nc'),
+                                'r')
         self.nstation = len(self.nc_to.variables['station'][:])
 
         # time vector for output data
         # get time and convert to datetime object
-        nctime = self.nc_pl.variables['time'][:]
-        self.t_unit = self.nc_pl.variables['time'].units  # "hours since 1900-01-01 00:00:0.0"
-        self.t_cal  = self.nc_pl.variables['time'].calendar
-        time = nc.num2date(nctime, units=self.t_unit, calendar=self.t_cal)
-
-        # interpolation scale factor
-        self.time_step = par['time_step'] * 3600    # [s] scaled file
-        self.interval_in = (time[1] - time[0]).seconds  # interval in seconds
-        self.interpN = floor(self.interval_in / self.time_step)
-
-        # number of time steps for output, include last value
-        self.nt = int(floor((max(time) - min(time)).total_seconds()
-                            / 3600 / par['time_step'])) + 1
-        
-
-        # vector of output time steps as datetime object
-        # 'seconds since 1900-01-01 00:00:0.0'
-        mt = min(time)
-        self.times_out = [mt + timedelta(seconds=(x * self.time_step))
-                          for x in range(0, self.nt)]
-
-        # vector of output time steps as written in ncdf file [s]
+        self.set_time_scale(self.nc_pl.variables['time'], par['time_step'])
         self.scaled_t_units = 'seconds since 1900-01-01 00:00:0.0'
-        self.times_out_nc = nc.date2num(self.times_out,
-                                        units=self.scaled_t_units,
-                                        calendar=self.t_cal)
+
+        self.times_out_nc = self.build_datetime_array(start_time=self.min_time,
+                                                      timestep_in_hours=self.time_step,
+                                                      num_times=self.nt,
+                                                      output_units=self.scaled_t_units,
+                                                      output_calendar=self.t_cal)
 
     def getValues(self, ncf, varStr, ni=10):
         if self.ens:
@@ -175,7 +153,8 @@ class ERA5scale(GenericScale):
         for n, s in enumerate(self.rg.variables['station'][:].tolist()):
             # scale from hPa to Pa
             self.rg.variables[vn][:, n] = series_interpolate(self.times_out_nc,
-                                          time_in * 3600, values[:, n]) * 100
+                                                             time_in * 3600,
+                                                             values[:, n]) * 100
 
     def AIRT_C_pl(self, ni=10):
         """

globsim/scale/ERAIscale.py
@@ -44,8 +44,7 @@ import netCDF4 as nc
 import numpy as np
 import logging
 
-from datetime import timedelta
-from math import floor, atan2, pi
+from math import atan2, pi
 from os import path
 from scipy.interpolate import interp1d
 
@@ -55,6 +54,7 @@ from globsim.nc_elements import new_scaled_netcdf
 from globsim.scale.GenericScale import GenericScale
 
 logger = logging.getLogger('globsim.scale')
+
 
 class ERAIscale(GenericScale):
     NAME = "ERA-I"
@@ -93,32 +93,14 @@ class ERAIscale(GenericScale):
 
         # time vector for output data
         # get time and convert to datetime object
-        nctime = self.nc_sf.variables['time'][:]
-        self.t_unit = self.nc_sf.variables['time'].units #"hours since 1900-01-01 00:00:0.0"
-        self.t_cal  = self.nc_sf.variables['time'].calendar
-        time = nc.num2date(nctime, units = self.t_unit, calendar = self.t_cal)
-
-        # interpolation scale factor
-        self.time_step = par['time_step'] * 3600    # [s] scaled file
-        interval_in = (time[1]-time[0]).seconds #interval in seconds
-        self.interpN = floor(interval_in/self.time_step)
-
-        #number of time steps for output, include last value
-        self.nt = int(floor((max(time) - min(time)).total_seconds()
-                      / 3600 / par['time_step']))+1
-
-        # vector of output time steps as datetime object
-        # 'seconds since 1900-01-01 00:00:0.0'
-        mt = min(time)
-        self.times_out = [mt + timedelta(seconds = (x*self.time_step))
-                          for x in range(0, self.nt)]
-
-        # vector of output time steps as written in ncdf file [s]
+        self.set_time_scale(self.nc_sf.variables['time'], par['time_step'])
         self.scaled_t_units = 'seconds since 1900-01-01 00:00:00'
-        self.times_out_nc = nc.date2num(self.times_out,
-                                        units = self.scaled_t_units,
-                                        calendar = self.t_cal)
-
+        
+        self.times_out_nc = self.build_datetime_array(start_time=self.min_time,
+                                                      timestep_in_hours=self.time_step,
+                                                      num_times=self.nt,
+                                                      output_units=self.scaled_t_units,
+                                                      output_calendar=self.t_cal)
 
     def process(self):
         """

globsim/scale/GenericScale.py
@@ -2,9 +2,13 @@ from __future__ import print_function
 
 import tomlkit
 import logging
+import numpy as np
+import netCDF4 as nc
 
+from datetime import datetime
 from os import path, makedirs
 from pathlib import Path
+from math import floor
 
 from globsim.common_utils import StationListRead
 
@@ -42,11 +46,11 @@ class GenericScale:
         self.kernels = par['kernels']
         if not isinstance(self.kernels, list):
             self.kernels = [self.kernels]
-        
+
         # should file be overwritten - default to false
         try:
             self._overwrite_output = par['overwrite']
-        except KeyError as e:
+        except KeyError:
             logger.warning("Missing overwrite parameter in control file. Reverting to default (ovewrite = true).")
             self._overwrite_output = False
         finally:
@@ -55,7 +59,7 @@ class GenericScale:
         # read snow correction info
         try:
             self.scf = par['scf']
-        except KeyError as e:
+        except KeyError:
             logger.warning("Missing snow correction factor parameter in control file. Reverting to default (scf = 1).")
             self.scf = 1
         finally:
@@ -84,19 +88,19 @@ class GenericScale:
     def make_output_directory(self, par):
         """make directory to hold outputs"""
         output_dir = None
-        
+
         if par.get('output_directory'):
             try:
                 test_path = Path(par.get('output_directory'))
             except TypeError:
                 msg = "You provided an output_directory for scaled files that does not exist. Saving files to project directory"
                 logger.warning(msg)
-            
+
             if test_path.is_dir():
                 output_dir = Path(test_path, "scaled")
             else:
                 logger.warning("You provided an output_directory for scaled files that was not understood. Saving files to project directory.")
-                
+
         if not output_dir:
             logger.debug(f"Attempting to create directory: {output_dir}")
             output_dir = path.join(par['project_directory'], 'scaled')
@@ -105,3 +109,36 @@ class GenericScale:
             makedirs(output_dir)
 
         return output_dir
+
+    def set_time_scale(self, time_variable, time_step):
+        nctime = time_variable[:]
+        self.t_unit = time_variable.units
+        self.t_cal  = time_variable.calendar
+        self.time_step = time_step
+        self.min_time = nc.num2date(min(nctime), units=self.t_unit, calendar=self.t_cal)
+
+        self.max_time = nc.num2date(max(nctime), units=self.t_unit, calendar=self.t_cal)
+        t1 = nc.num2date(nctime[1], units=self.t_unit, calendar=self.t_cal)
+        t0 = nc.num2date(nctime[0], units=self.t_unit, calendar=self.t_cal)
+        self.interval_in = (t1 - t0).seconds
+
+        # number of time steps
+        self.nt = floor((self.max_time - self.min_time).total_seconds() / (3600 * time_step)) + 1
+        logger.debug(f"Output time array has {self.nt} elements between "
+                     f"{self.min_time.strftime('%Y-%m-%d %H:%M:%S')} and "
+                     f"{self.max_time.strftime('%Y-%m-%d %H:%M:%S')}"
+                     f" (time step of {self.time_step} hours)")
+
+    @staticmethod
+    def build_datetime_array(start_time: datetime, timestep_in_hours: int, num_times: int, output_units:str, output_calendar:str):
+        time_array = np.arange(num_times, dtype='float64') * timestep_in_hours * 3600
+
+        datetime_array = nc.num2date(time_array,
+                                     units=f"seconds since {start_time.strftime('%Y-%m-%d %H:%M:%S.%f')}",
+                                     calendar="gregorian")
+
+        result = nc.date2num(datetime_array,
+                             units=output_units,
+                             calendar=output_calendar)
+
+        return result

globsim/scale/JRAscale.py
@@ -6,9 +6,8 @@ import netCDF4 as nc
 import numpy as np
 import logging
 
-from datetime import timedelta
-from os import path
-from math import floor, atan2, pi
+from pathlib import Path
+from math import atan2, pi
 from scipy.interpolate import interp1d
 
 from globsim.common_utils import str_encode, series_interpolate
@@ -17,6 +16,7 @@ from globsim.nc_elements import new_scaled_netcdf
 from globsim.scale.GenericScale import GenericScale
 
 logger = logging.getLogger('globsim.scale')
+
 
 class JRAscale(GenericScale):
     """
@@ -39,33 +39,25 @@ class JRAscale(GenericScale):
         par = self.par
 
         # input file names
-        self.nc_pl = nc.Dataset(path.join(self.intpdir,'jra_pl_' +
-                                self.list_name + '_surface.nc'), 'r')
-        self.nc_sa = nc.Dataset(path.join(self.intpdir,'jra_sa_' +
-                                self.list_name + '.nc'), 'r')
-        self.nc_sf = nc.Dataset(path.join(self.intpdir,'jra_sf_' +
-                                self.list_name + '.nc'), 'r')
+        self.nc_pl = nc.Dataset(Path(self.intpdir, f'jra_pl_{self.list_name}_surface.nc'),
+                                'r')
+        self.nc_sa = nc.Dataset(Path(self.intpdir, f'jra_sa_{self.list_name}.nc'),
+                                'r')
+        self.nc_sf = nc.Dataset(Path(self.intpdir, f'jra_sf_{self.list_name}.nc'),
+                                'r')
 
         # check if output file exists and remove if overwrite parameter is set
         self.output_file = self.getOutNCF(par, 'jra55')
 
         # time vector for output data
         # get time and convert to datetime object
-        nctime = self.nc_pl.variables['time'][:]
-        self.t_unit = self.nc_pl.variables['time'].units
-        self.t_cal  = self.nc_pl.variables['time'].calendar
-        time = nc.num2date(nctime, units=self.t_unit, calendar=self.t_cal)
+        self.set_time_scale(self.nc_pl.variables['time'], par['time_step'])
 
-        # number of time steps
-        self.nt = floor((max(time) - min(time)).total_seconds() / 3600 / par['time_step']) + 1
-        self.time_step = par['time_step']
-
-        # vector of output time steps as datetime object
-        self.times_out    = [min(time) + timedelta(hours=x * par['time_step'])
-                             for x in range(0, self.nt)]
-        # vector of output time steps as written in ncdf file
-        self.times_out_nc = nc.date2num(self.times_out, units=self.t_unit,
-                                        calendar=self.t_cal)
+        self.times_out_nc = self.build_datetime_array(start_time=self.min_time,
+                                                      timestep_in_hours=self.time_step,
+                                                      num_times=self.nt,
+                                                      output_units=self.t_unit,
+                                                      output_calendar=self.t_cal)
 
     def process(self):
         """
@@ -172,7 +164,7 @@ class JRAscale(GenericScale):
 
         # interpolate station by station
         time_in = self.nc_sa.variables['time'][:]
-        values  = self.nc_sa.variables['Relative humidity'][:]/100
+        values  = self.nc_sa.variables['Relative humidity'][:] / 100
         for n, s in enumerate(self.rg.variables['station'][:].tolist()):
             self.rg.variables[vn][:, n] = np.interp(self.times_out_nc,
                                                     time_in, values[:, n])

globsim/scale/MERRAscale.py
@@ -97,11 +97,10 @@ from __future__        import print_function
 import netCDF4 as nc
 import numpy as np
 import warnings
-import logging 
+import logging
 
-from datetime  import timedelta
 from os import path, makedirs
-from math import floor, atan2, pi
+from math import atan2, pi
 from scipy.interpolate import interp1d
 
 from globsim.common_utils import str_encode, series_interpolate
@@ -148,32 +147,14 @@ class MERRAscale(GenericScale):
 
         # time vector for output data
         # get time and convert to datetime object
-        nctime = self.nc_pl.variables['time'][:]
-        self.t_unit = self.nc_pl.variables['time'].units  # "hours since 1980-01-01 00:00:00"
-        self.t_cal  = self.nc_pl.variables['time'].calendar
-        time = nc.num2date(nctime, units=self.t_unit, calendar=self.t_cal)
-
-        # interpolation scale factor
-        self.time_step = par['time_step'] * 3600    # [s] scaled file
-        interval_in = (time[1] - time[0]).seconds  # interval in seconds
-        self.interpN = floor(interval_in / self.time_step)
-
-        # number of time steps for output
-        self.nt = int(floor((max(time) - min(time)).total_seconds()
-                      / 3600 / par['time_step'])) + 1  # +1 : include last value
-        self.time_step = par['time_step'] * 3600    # [s] scaled file
-
-        # vector of output time steps as datetime object
-        # 'seconds since 1980-01-01 00:00:00'
-        mt = min(time)
-        self.times_out = [mt + timedelta(seconds=(x * self.time_step))
-                          for x in range(0, self.nt)]
-
-        # vector of output time steps as written in ncdf file
+        self.set_time_scale(self.nc_pl.variables['time'], par['time_step'])
         self.scaled_t_units = 'seconds since 1980-01-01 00:00:00'
-        self.times_out_nc = nc.date2num(self.times_out,
-                                        units=self.scaled_t_units,
-                                        calendar=self.t_cal)
+
+        self.times_out_nc = self.build_datetime_array(start_time=self.min_time,
+                                                      timestep_in_hours=self.time_step,
+                                                      num_times=self.nt,
+                                                      output_units=self.scaled_t_units,
+                                                      output_calendar=self.t_cal)
 
     def process(self):
         """