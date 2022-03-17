77909fdfcc2c22b11d2918ca07dda2152b7f30e8
Nick Brownn <nicholas512@gmail.com>
2022-03-17 20:37:25 +0000

    switch ERA5 over to new monthly threadded download

--- globsim/download/__init__.py
+++ globsim/download/__init__.py
@@ -2,3 +2,4 @@ from .ERA5download import ERA5download
 from .ERAIdownload import ERAIdownload
 from .JRAdownload import JRAdownload
 from .MERRAdownload import MERRAdownload
+from .era5_monthly import download_threadded, ERA5MonthlyDownload
