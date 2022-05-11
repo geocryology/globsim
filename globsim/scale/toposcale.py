b5cadef150ab461ea32a369d277318c979f22639
nicholas512 <15223327+nicholas512@users.noreply.github.com>
2022-05-11 11:32:52 -0600

    add docstrings to toposcale

--- globsim/scale/toposcale.py
+++ globsim/scale/toposcale.py
@@ -1,21 +1,39 @@
 from globsim.meteorology import emissivity_clear_sky, boltzmann
+from typing import Union
+import numpy as np
 
 
-def emissivity_all_sky(lw_grid, t_grid):
-    """ Eq (2) from Fiddes & Gruber """
-    e_all_sky = lw_grid / (boltzmann * t_grid ** 4)
+def emissivity_all_sky(lw: "Union[float, np.ndarray]",
+                       t: "Union[float, np.ndarray]") -> "Union[float, np.ndarray]":
+    """ Eq (2) from Fiddes & Gruber 
+    lw : float
+        longwave radiation [W m-2] 
+    t : float
+        temperature [K] 
+    """
+    e_all_sky = lw / (boltzmann * np.power(t, 4))
 
     return e_all_sky
 
 
-def emissivity_cloud(e_cl_grid, e_as_grid):
-    """ Estimate of cloud-based component of emissivity """
-    delta_e = e_as_grid - e_cl_grid
+def emissivity_cloud(e_cl: "Union[float, np.ndarray]", 
+                     e_as: "Union[float, np.ndarray]") -> "Union[float, np.ndarray]":
+    """ Estimate of cloud-based component of emissivity
+    e_cl : float
+        clear-sky component of emissivity
+    e_as : float
+        all-sky component of emissivity
+    """
+    delta_e = e_as - e_cl
 
     return delta_e
 
 
-def lw_down_toposcale(t_sub, rh_sub, t_sur, rh_sur, lw_sur):
+def lw_down_toposcale(t_sub: "Union[float, np.ndarray]", 
+                      rh_sub: "Union[float, np.ndarray]",
+                      t_sur: "Union[float, np.ndarray]",
+                      rh_sur: "Union[float, np.ndarray]",
+                      lw_sur: "Union[float, np.ndarray]") -> "Union[float, np.ndarray]":
     """ Eq (3) From Fiddes & Gruber
 
     Parameters
@@ -29,7 +47,7 @@ def lw_down_toposcale(t_sub, rh_sub, t_sur, rh_sur, lw_sur):
     rh_sur : float
         relative humidity [1] (unitless) at surface level (GRID level)
     lw_sur : float
-        total downwelling LW radiation at surface level
+        total downwelling LW radiation [W m-2] at surface level (GRID level)
     """
     clear_sky_emissivity_sub = emissivity_clear_sky(rh_sub, t_sub)
     clear_sky_emissivity_sur = emissivity_clear_sky(rh_sur, t_sur)
@@ -37,6 +55,6 @@ def lw_down_toposcale(t_sub, rh_sub, t_sur, rh_sur, lw_sur):
 
     delta_e = emissivity_cloud(clear_sky_emissivity_sur, all_sky_emissivity_sur)
 
-    lw_d_sub = (clear_sky_emissivity_sub + delta_e) * boltzmann * t_sub ** 4
+    lw_d_sub = (clear_sky_emissivity_sub + delta_e) * boltzmann * np.power(t_sub, 4)
 
     return lw_d_sub
